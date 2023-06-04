c20f10fb95c0cf3fbef2f962f25162249e20040c
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 19:13:35 +0200

    array toys

--- database/db.php
+++ database/db.php
@@ -3,20 +3,15 @@
 include_once __DIR__ . "/../models/product.php";
 include_once __DIR__ . "/../models/category.php";
 include_once __DIR__ . "/../models/food.php";
+include_once __DIR__ . "/../models/toy.php";
 
 
-$prova = new Product(0, 'prodotto 1',
-'https://picsum.photos/id/837/300/200',
-[ new Category('cane') ] );
 
-// $prova->setPrice(30);
-
-var_dump($prova);
-
+// --- ARRAY CIBO ---
 $arrayFood = [
     // Istanza Food 1
-    $food2 = new Food(
-        1,
+    $food1 = new Food(
+        0,
         'Crocchette Premium',
         'https://picsum.photos/id/169/300/200',
         [new Category('cane')],
@@ -25,7 +20,7 @@ $arrayFood = [
     ),
     // Istanza Food 2
     $food2 = new Food(
-        2,
+        1,
         'Paté di Tonno',
         'https://picsum.photos/id/360/300/200',
         [new Category('gatto')],
@@ -35,7 +30,7 @@ $arrayFood = [
 
     // Istanza Food 3
     $food3 = new Food(
-        3,
+        2,
         'Snack al Pollo',
         'https://picsum.photos/id/802/300/200',
         [new Category('cane'), new Category('gatto')],
@@ -44,13 +39,43 @@ $arrayFood = [
     ),
 ];
 
-var_dump($arrayFood);
-
 // uso la funzione della classe food per stabilire i prezzi di ogni istanza
 foreach ($arrayFood as $elem) {
     $elem->setPrice();
 };
+// var_dump($arrayFood);
 
-var_dump($arrayFood);
 
+// --- ARRAY TOYS ---
+$arrayToy = [
+
+    $toy1 = new Toy(
+        0,
+        'Palla Rimbalzante',
+        'https://picsum.photos/id/173/300/200',
+        [new Category('cane'), new Category('gatto')],
+        'gomma',
+        'm'
+    ),
+
+    $toy2 = new Toy(
+        1,
+        'Giochino con Piume',
+        'https://picsum.photos/id/174/300/200',
+        [new Category('gatto')],
+        'piume',
+        's'
+    ),
+
+    $toy3 = new Toy(
+        2,
+        'Frisbee Resistente',
+        'https://picsum.photos/id/175/300/200',
+        [new Category('cane')],
+        'plastica',
+        'xl'
+    ),
+];
+
+var_dump($arrayToy);
 ?>
