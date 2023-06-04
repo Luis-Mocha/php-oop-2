a4faf2b8e933c6b40de16156c1e8024ffefdcccf
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 18:25:54 +0200

    stampo l'arrayFood tramite classe bootstrap card

--- partials/main.php
+++ partials/main.php
@@ -1,7 +1,21 @@
 <main class="">
-    
-<p>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis officiis non quam unde architecto dolorum, et neque doloremque maxime in necessitatibus maiores laudantium eveniet, autem ex sed tenetur quaerat quos.
-</p>
+    <div class="container">
 
+        <!-- SEZIONE FOOD -->
+        <div class="card-container">
+            <?php foreach ($arrayFood as $elem) { ?>
+                <div class="card">
+                    <img src=" <?php echo $elem->img ?> " class="card-img-top" alt="<?php echo $elem->name . ' Immagine' ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $elem->name ?> </h5>
+                        <p class="card-text"> Tipo: <?php echo $elem->type ?> </p>
+                        <p class="card-text"> Quantità: <?php echo $elem->quantity ?> g </p>
+
+                    </div>
+                    </div>
+            <?php } ?>
+        </div>
+
+
+    </div>
 </main>
