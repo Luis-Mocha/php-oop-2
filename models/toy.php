908ca296fc8dda07301d644eca2e73c64b0f984d
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 19:13:13 +0200

    classe toy

---
+++ models/toy.php
@@ -0,0 +1,15 @@
+<?php
+
+class Toy extends Product {
+    public $material;
+    public $dimension; // s, m , l , xl
+    
+
+    public function __construct($_id, $_name, $_img, $_category, $_material, $_dimension) {
+        parent::__construct($_id, $_name, $_img, $_category);
+
+        $this->material = $_material;
+        $this->dimension = $_dimension;
+    }
+
+}
