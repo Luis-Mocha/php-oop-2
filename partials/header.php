cd861fc1d80e692a8c4fe85f813b0e421f319498
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 18:49:21 +0200

    header title

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,12 @@
 <header>
     <nav  class="container">
-        <div class="fs-1 fst-italic">
-            Header
+        <div class="fs-1 fst-italic fw-bold text-end">
+            <span>
+                <i class="fa-solid fa-shield-dog"></i>
+            </span>
+            <span>
+                Shop
+            </span>
         </div>
     </nav>
 </header>
