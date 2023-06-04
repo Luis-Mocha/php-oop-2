59e388ac61075b33244230028654a14eef5a4b81
Luis-Mocha <luismocha97@gmail.com>
2023-06-04 19:36:06 +0200

    stampo card cuccie

--- partials/main.php
+++ partials/main.php
@@ -55,7 +55,43 @@
                             } ?> 
                         </div>
 
-                        <div class="card-text"> Materiale:
+                        <div class="card-text"> Dimensioni:
+                            <span class="text-uppercase">
+                                <?php echo $elem->dimension ?>
+                            </span>
+                        </div>
+
+                        <div class="card-text text-end fw-bold fst-italic"> <?php echo $elem->price ?> € </div>
+
+                    </div>
+                </div>
+
+            <?php } ?>
+        </div>
+
+        <!-- SEZIONE CUCCIE -->
+        <div class="card-container">
+
+            <?php foreach ($arrayBed as $elem) { ?>
+
+                <div class="card">
+                    <img src=" <?php echo $elem->img ?> " class="card-img-top" alt="<?php echo $elem->name . ' Immagine' ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $elem->name ?> </h5>
+                        <div class="card-text"> Colore: <?php echo $elem->color ?> </div>
+                        
+                        <div class="card-text"> Categoria:
+                            <?php foreach ($elem->category as $elemCategory) { 
+                                if ($elemCategory->name == 'cane') { ?>
+                                    <span class="ms-2"> <i class="fa-solid fa-dog"></i> </span>
+                                <?php } 
+                                if ($elemCategory->name == 'gatto') { ?>
+                                    <span class="ms-2"> <i class="fa-solid fa-cat"></i> </span>
+                                <?php }
+                            } ?> 
+                        </div>
+
+                        <div class="card-text"> Dimensioni:
                             <span class="text-uppercase">
                                 <?php echo $elem->dimension ?>
                             </span>
