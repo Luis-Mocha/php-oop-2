141ec69d7153716536be13693ebf5bc7fab3b10b
Luis-Mocha <luismocha97@gmail.com>
2023-06-05 19:11:42 +0200

    implemento il tratto dimension

--- models/bed.php
+++ models/bed.php
@@ -1,9 +1,11 @@
 <?php
 
 class Bed extends Product {
+
+    use Dimension; // s, m , l , xl
     public $color;
-    public $dimension; // s, m , l , xl
-    
+    // public $dimension;
+
 
     public function __construct($_id, $_name, $_img, $_category, $_color, $_dimension) {
         parent::__construct($_id, $_name, $_img, $_category);
--- models/toy.php
+++ models/toy.php
@@ -1,9 +1,11 @@
 <?php
 
 class Toy extends Product {
+
+    use Dimension; // s, m , l , xl
     public $material;
-    public $dimension; // s, m , l , xl
-    
+    // public $dimension;
+
 
     public function __construct($_id, $_name, $_img, $_category, $_material, $_dimension) {
         parent::__construct($_id, $_name, $_img, $_category);
@@ -11,5 +13,5 @@ class Toy extends Product {
         $this->material = $_material;
         $this->dimension = $_dimension;
     }
-    
+
 }
