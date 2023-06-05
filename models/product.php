ea800096ac3a9572e85128ac1f30903fa30c3fd9
Luis-Mocha <luismocha97@gmail.com>
2023-06-05 16:56:56 +0200

    funzione setPrice unica in classe Product

--- models/product.php
+++ models/product.php
@@ -15,4 +15,33 @@ class Product {
         $this->category = $_category;
     }
 
+    public function newPrice(){
+        if (get_class($this) === 'Toy') {
+            if ($this->dimension == 's') {
+                $this->price = 10;
+            } else if ($this->dimension == 'm') {
+                $this->price = 25;
+            } else {
+                $this->price = 50;
+            } 
+        } else if (get_class($this) === 'Bed') {
+            if ($this->dimension == 's') {
+                $this->price = 30;
+            } else if ($this->dimension == 'm') {
+                $this->price = 50;
+            } else {
+                $this->price = 100;
+            } 
+        } else {
+            if ($this->quantity <= 200) {
+                $this->price = 2;
+            } else if ($this->quantity > 200 && $this->quantity <= 1000) {
+                $this->price = 5;
+            } else {
+                $this->price = 10;
+            }  
+        }
+        
+    }
+
 }
